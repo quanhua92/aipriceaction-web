f611ab4d5c2789f8ab7ecf75c5b8cf5e8bff9e80
Quan Hua <quanhua92@gmail.com>
2025-11-11 19:47:28 +0700

    fix: Disable TanStack DevTools in production builds

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -71,18 +71,20 @@ function RootDocument({ children }: { children: React.ReactNode }) {
             <ChartSettingsProvider>
               <Header />
               {children}
-            <TanStackDevtools
-              config={{
-                position: 'bottom-right',
-              }}
-              plugins={[
-                {
-                  name: 'Tanstack Router',
-                  render: <TanStackRouterDevtoolsPanel />,
-                },
-                TanStackQueryDevtools,
-              ]}
-            />
+            {import.meta.env.DEV && (
+              <TanStackDevtools
+                config={{
+                  position: 'bottom-right',
+                }}
+                plugins={[
+                  {
+                    name: 'Tanstack Router',
+                    render: <TanStackRouterDevtoolsPanel />,
+                  },
+                  TanStackQueryDevtools,
+                ]}
+              />
+            )}
             </ChartSettingsProvider>
           </APIProvider>
         </SiteSettingsProvider>
